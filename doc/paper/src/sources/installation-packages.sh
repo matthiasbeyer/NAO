548cc3fec2615e24c847e82e94da4df82374c15c
Matthias Beyer <mail@beyermatthias.de>
2015-10-27 13:37:44 +0100

    Add installation script

---
+++ doc/paper/src/sources/installation-packages.sh
@@ -0,0 +1,23 @@
+sudo apt-get install \
+    cmake \
+    gcc \
+    git \
+    libavcodec-dev \
+    libavformat-dev \
+    libdc1394-22-dev \
+    libgstreamer-plugins-base0.10-dev \
+    libgstreamer0.10-dev \
+    libjasper-dev \
+    libjpeg-dev \
+    libopencv-dev
+    libqt4libgtk2.0-dev \
+    libswscale-dev \
+    libtbb-dev \
+    libtiff4-dev
+    libv4l-dev \
+    libxine-dev \
+    python-dev \
+    python-numpy \
+    python-pip \
+    qtcreator
+
