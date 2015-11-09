2245abb47a072eaaf5e1ba5a82cd3ac4357304b3
Matthias Beyer <mail@beyermatthias.de>
2015-11-09 19:23:17 +0100

    Cleanup code

--- sources/src/opencv/opencv/Objekte.cpp
+++ sources/src/opencv/opencv/Objekte.cpp
@@ -2,64 +2,59 @@
 
 Objekte::Objekte()
 {
-	//set values for default constructor
-	setType("Objekte");
-	setColor(Scalar(0,0,0));
-
+    //set values for default constructor
+    setType("Objekte");
+    setColor(Scalar(0,0,0));
 }
 
 Objekte::Objekte(string name){
 
-	setType(name);
+    setType(name);
 
-	if(name=="blue"){
+    if(name=="blue"){
+        //TODO: use "calibration mode" to find HSV min
+        //and HSV max values
 
-		//TODO: use "calibration mode" to find HSV min
-		//and HSV max values
+        setHSVmin(Scalar(92,0,0));
+        setHSVmax(Scalar(124,256,256));
 
-		setHSVmin(Scalar(92,0,0));
-		setHSVmax(Scalar(124,256,256));
+        //BGR value for Green:
+        setColor(Scalar(255,0,0));
+    }
 
-		//BGR value for Green:
-		setColor(Scalar(255,0,0));
+    if(name=="green"){
+        //TODO: use "calibration mode" to find HSV min
+        //and HSV max values
 
-	}
-	if(name=="green"){
+        setHSVmin(Scalar(34,50,50));
+        setHSVmax(Scalar(80,220,200));
 
-		//TODO: use "calibration mode" to find HSV min
-		//and HSV max values
+        //BGR value for Yellow:
+        setColor(Scalar(0,255,0));
+    }
 
-		setHSVmin(Scalar(34,50,50));
-		setHSVmax(Scalar(80,220,200));
+    if(name=="yellow"){
+        //TODO: use "calibration mode" to find HSV min
+        //and HSV max values
 
-		//BGR value for Yellow:
-		setColor(Scalar(0,255,0));
+        setHSVmin(Scalar(20,124,123));
+        setHSVmax(Scalar(30,256,256));
 
-	}
-	if(name=="yellow"){
+        //BGR value for Red:
+        setColor(Scalar(0,255,255));
+    }
 
-		//TODO: use "calibration mode" to find HSV min
-		//and HSV max values
+    if(name=="red"){
+        //TODO: use "calibration mode" to find HSV min
+        //and HSV max values
 
-		setHSVmin(Scalar(20,124,123));
-		setHSVmax(Scalar(30,256,256));
+        setHSVmin(Scalar(0,200,0));
+        setHSVmax(Scalar(19,255,255));
 
-		//BGR value for Red:
-		setColor(Scalar(0,255,255));
+        //BGR value for Red:
+        setColor(Scalar(0,0,255));
+    }
 
-	}
-	if(name=="red"){
-
-		//TODO: use "calibration mode" to find HSV min
-		//and HSV max values
-
-		setHSVmin(Scalar(0,200,0));
-		setHSVmax(Scalar(19,255,255));
-
-		//BGR value for Red:
-		setColor(Scalar(0,0,255));
-
-	}
 }
 
 Objekte::~Objekte(void)
@@ -67,46 +62,34 @@ Objekte::~Objekte(void)
 }
 
 int Objekte::getXPos(){
-
-	return Objekte::xPos;
-
+    return Objekte::xPos;
 }
 
 void Objekte::setXPos(int x){
-
-	Objekte::xPos = x;
-
+    Objekte::xPos = x;
 }
 
 int Objekte::getYPos(){
-
-	return Objekte::yPos;
-
+    return Objekte::yPos;
 }
 
 void Objekte::setYPos(int y){
-
-	Objekte::yPos = y;
-
+    Objekte::yPos = y;
 }
 
 Scalar Objekte::getHSVmin(){
-
-	return Objekte::HSVmin;
-
+    return Objekte::HSVmin;
 }
-Scalar Objekte::getHSVmax(){
 
-	return Objekte::HSVmax;
+Scalar Objekte::getHSVmax(){
+    return Objekte::HSVmax;
 }
 
 void Objekte::setHSVmin(Scalar min){
-
-	Objekte::HSVmin = min;
+    Objekte::HSVmin = min;
 }
-
 
 void Objekte::setHSVmax(Scalar max){
-
-	Objekte::HSVmax = max;
+    Objekte::HSVmax = max;
 }
+
