e76fb1d6add6fa2ce56cf6b05cf71f83b81bbede
Matthias Beyer <mail@beyermatthias.de>
2015-11-18 13:49:52 +0100

    Make symbols globally available

--- sources/src/opencv/opencv/module.hpp
+++ sources/src/opencv/opencv/module.hpp
@@ -4,6 +4,12 @@
 
 namespace naocv {
 
+    extern int lowThreshold;
+    extern Mat dst;
+    extern Mat detected_edges;
+    extern Mat src;
+    extern Mat src_gray;
+
     int run(const std::string& pathToFile,
             bool calibrationMode = true,
             bool videoMode = false);
