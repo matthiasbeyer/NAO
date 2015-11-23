d33ee7ae281d94bf8207a29e2686b899161d23a0
Matthias Beyer <mail@beyermatthias.de>
2015-11-23 15:48:02 +0100

    Add high-level main

---
+++ sources/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * main.cpp
+ *
+ * Main file which contains the main routine. We do the high-level stuff here.
+ *
+ */
+
+int main(void)
+{
+    // Introduction
+    //
+    // Find Cube
+    //
+    // Go to cube
+    //
+    // {
+    //      Throw cube
+    //
+    //      find/goto cube
+    //
+    //      analyze cube-color
+    //
+    //      Game step
+    //
+    // } as long as game algo says yes
+    //
+    // Outro
+}
